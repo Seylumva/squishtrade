4107bb26b228ff6ac13f1265b8e034cff0ccceb1
SeylumVA <sc2nem@gmail.com>
2022-03-17 03:06:58 -0400

    added: Home page content and styles

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,9 @@
 import Page from "../components/Page";
 import styles from "./Home.module.css";
 import { Helmet } from "react-helmet-async";
+import { Link } from "react-router-dom";
+import Patty from "../assets/Patty.png";
+import Avery from "../assets/Avery.webp";
 
 const Home = () => {
   return (
@@ -10,12 +13,47 @@ const Home = () => {
       </Helmet>
       <Page fluid={false}>
         <div className={styles.hero}>
-          <h2>Welcome to Squishtrade!</h2>
+          <h2>
+            Find your ISOs and DISOs for retail prices from fellow collectors
+          </h2>
           <p>
-            Home of retail priced Squishmallow sold and traded by fellow
-            collectors.
+            Join a community-driven marketplace with the purpose to rehome
+            Squishmallow to real collectiors
           </p>
+          <div className={styles.links}>
+            <Link to="/about">Learn More</Link>
+            <Link to="/listing/new">Start Listing</Link>
+          </div>
+          <img
+            src={Patty}
+            alt="Patty the Cow Squishmallow"
+            className={styles.patty}
+          />
         </div>
+      </Page>
+      <Page fluid={false}>
+        <section className={styles.services}>
+          <article className={styles.service}>
+            <h3>No fees, no catch</h3>
+            <p>
+              We do not collect any data besides what's used to display listings
+              and allow for communication between users. It is solely for the
+              purpose of this website.
+            </p>
+          </article>
+          <article className={styles.service}>
+            <h3>Fair prices</h3>
+            <p>
+              We moderate listings to only allow them to be reasonably priced,
+              guided by official retailer's prices.
+            </p>
+          </article>
+          <img
+            src={Avery}
+            alt="Avery the Duck Squishmallow"
+            className={styles.avery}
+          />
+        </section>
       </Page>
     </>
   );
