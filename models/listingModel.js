5eeead83e2bdf8362c80539c2b119a7b941188e8
SeylumVA <sc2nem@gmail.com>
2022-03-19 23:52:15 -0400

    added: Timestamps to listing model

--- models/listingModel.js
+++ models/listingModel.js
@@ -1,37 +1,42 @@
 const mongoose = require("mongoose");
 
-const listingSchema = mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
+const listingSchema = mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    type: {
+      type: String,
+      enum: ["Standard", "Stack", "Squeezemallow", "Tin"],
+      required: true,
+    },
+    condition: {
+      type: String,
+      enum: ["Brand New", "Like New", "Used"],
+      required: true,
+    },
+    isPublic: {
+      type: Boolean,
+      default: true,
+    },
   },
-  description: {
-    type: String,
-    required: true,
-  },
-  price: {
-    type: Number,
-    required: true,
-  },
-  author: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
-  },
-  type: {
-    type: String,
-    enum: ["Standard", "Stack", "Squeezemallow", "Tin"],
-    required: true,
-  },
-  condition: {
-    type: String,
-    enum: ["Brand New", "Like New", "Used"],
-    required: true,
-  },
-  isPublic: {
-    type: Boolean,
-    default: true,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = mongoose.model("Listing", listingSchema);
