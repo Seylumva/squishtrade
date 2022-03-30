c7138b737b9107d3586f533adee21149cf759083
SeylumVA <sc2nem@gmail.com>
2022-03-30 04:26:19 -0400

    added: Index exports for components folder

---
+++ client/src/components/index.js
@@ -0,0 +1,5 @@
+export { default as PrivateRoute } from "./PrivateRoute";
+export { default as SignOutButton } from "./SignOutButton";
+export { default as Header } from "./Header";
+export { default as Spinner } from "./Spinner";
+export { default as AuthorStats } from "./AuthorStats";
