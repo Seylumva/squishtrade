55279b4957c5fd58a584507d39c06645023a6708
SeylumVA <sc2nem@gmail.com>
2022-04-05 01:32:38 -0400

    Added keys to ListingsList component

--- client/src/components/ListingsList.jsx
+++ client/src/components/ListingsList.jsx
@@ -5,7 +5,9 @@ const ListingsList = ({ listings }) => {
     <div className="px-5 mx-auto container grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4  xl:grid-cols-5 gap-5">
       {listings &&
         listings.length > 0 &&
-        listings.map((listing) => <ListingsCard listing={listing} />)}
+        listings.map((listing) => (
+          <ListingsCard key={listing._id} listing={listing} />
+        ))}
     </div>
   );
 };
